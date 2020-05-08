4413de0b8fbc3267437b8f0f5408ee7d79a85258
quan200699 <51079189+quan200699@users.noreply.github.com>
2020-05-08 22:48:50 +0700

    add cities to temperatures property

--- src/main/java/com/codegym/demo/model/Temperatures.java
+++ src/main/java/com/codegym/demo/model/Temperatures.java
@@ -2,10 +2,7 @@ package com.codegym.demo.model;
 
 import lombok.Data;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 @Data
@@ -15,4 +12,7 @@ public class Temperatures {
     private Long id;
 
     private String temperature;
+
+    @ManyToOne
+    private Cities cities;
 }
