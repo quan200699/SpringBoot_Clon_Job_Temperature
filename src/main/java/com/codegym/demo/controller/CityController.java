1bea454514104a3251062bdfe8fb456e6c8a3e7d
quan200699 <51079189+quan200699@users.noreply.github.com>
2020-05-08 22:13:54 +0700

    create new city done

--- src/main/java/com/codegym/demo/controller/CityController.java
+++ src/main/java/com/codegym/demo/controller/CityController.java
@@ -5,10 +5,7 @@ import com.codegym.demo.service.ICityService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @CrossOrigin("*")
@@ -20,5 +17,10 @@ public class CityController {
     @GetMapping
     public ResponseEntity<Iterable<City>> getAllCity() {
         return new ResponseEntity<>(cityService.findAll(), HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<City> createNewCity(@RequestBody City city) {
+        return new ResponseEntity<>(cityService.save(city), HttpStatus.OK);
     }
 }
