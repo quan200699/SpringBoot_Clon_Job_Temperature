11942a1d3fcfb20a4f403a4198f4e629249c8b3b
quan200699 <51079189+quan200699@users.noreply.github.com>
2020-05-11 10:17:44 +0700

    add user entity

---
+++ src/main/java/com/codegym/demo/model/User.java
@@ -0,0 +1,18 @@
+package com.codegym.demo.model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String userId;
+
+    private boolean enable;
+}
