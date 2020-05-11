135e09310747fd2a07eea2b2408984de16c52489
quan200699 <51079189+quan200699@users.noreply.github.com>
2020-05-11 13:31:28 +0700

    delete auto inc

--- src/main/java/com/codegym/demo/model/User.java
+++ src/main/java/com/codegym/demo/model/User.java
@@ -11,7 +11,6 @@ import javax.persistence.*;
 @Table(name = "users")
 public class User {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private boolean enable;
